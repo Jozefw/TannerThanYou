cafaaa7d05b5f777f4176d9f3f2c91b05943cc3b
Jozef <Jozefw@sbcglobal.net>
2025-01-20 09:36:07 -0800

    getting page numbers

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -5,12 +5,12 @@ import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   const {data, isError, error, isLoading } = useQuery({
-    queryKey:["posts"],
-    queryFn:fetchPosts,
+    queryKey:["posts",currentPage],
+    queryFn:()=>fetchPosts(currentPage),
     staleTime:2000,
   });
 
@@ -30,11 +30,11 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <=1} onClick={() => {setCurrentPage((prevValue)=>prevValue - 1)}}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage  >= maxPostPage} onClick={() => {setCurrentPage((prevValue)=>prevValue + 1)}}>
           Next page
         </button>
       </div>
