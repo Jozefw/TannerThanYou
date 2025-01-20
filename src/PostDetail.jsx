2cf6d6a82eee7a5a88f2f33b6e7ab5ee733f3e6d
Jozef <Jozefw@sbcglobal.net>
2025-01-19 16:29:58 -0800

    add Post and comment fetches including dependancy array

---
+++ src/PostDetail.jsx
@@ -0,0 +1,35 @@
+import { fetchComments } from "./api";
+import { useQuery } from "@tanstack/react-query";
+import "./PostDetail.css";
+
+export function PostDetail({ post }) {
+  // replace with useQuery
+  const {data, isError, isLoading} = useQuery({
+    queryKey:["comments",post.id],
+    queryFn:()=>fetchComments(post.id),
+    staleTime:2000
+  });
+
+
+  if(isLoading){
+    return(<h3>...Loading...</h3>)
+  }
+
+if(isError){ 
+return(<h3>Something is wrong. Error:</h3>)}
+
+
+  return (
+    <>
+      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <button>Delete</button> <button>Update title</button>
+      <p>{post.body}</p>
+      <h4>Comments</h4>
+      {data.map((comment) => (
+        <li key={comment.id}>
+          {comment.email}: {comment.body}
+        </li>
+      ))}
+    </>
+  );
+}
